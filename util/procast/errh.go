97b779bda4c5239ae4734c7702e00c6aaf7df426
bagaking <zfaustk@qq.com>
2021-09-04 02:30:41 +0800

    feat: create procast/errhandler

---
+++ util/procast/errh.go
@@ -0,0 +1,35 @@
+package procast
+
+type (
+	ErrorHandler func(err error)
+)
+
+var EHEmpty ErrorHandler = func(error) {}
+
+func GetRewriteErrHandler(err *error) ErrorHandler {
+	if err == nil {
+		return EHEmpty
+	}
+	return func(e error) {
+		*err = e
+	}
+}
+
+func GetWrapVoidErrHandler(fn func()) ErrorHandler {
+	if fn == nil {
+		return EHEmpty
+	}
+	return func(e error) {
+		fn()
+	}
+}
+
+func GetCombinedErrHandler(handlers ...ErrorHandler) ErrorHandler {
+	return func(e error) {
+		for _, h := range handlers {
+			if h != nil {
+				h(e)
+			}
+		}
+	}
+}
