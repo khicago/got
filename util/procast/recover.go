27c5c5afea7b785466e4504c69b92e8ed389896c
bagaking <zfaustk@qq.com>
2022-07-03 14:22:06 +0800

    refactor (procast): implement recover using callerframes

--- util/procast/recover.go
+++ util/procast/recover.go
@@ -2,73 +2,48 @@ package procast
 
 import (
 	"fmt"
-	"runtime"
-	"strings"
+	"runtime/debug"
 )
-
-func figurePanicLocation(skipExtra int) string {
-	var frame runtime.Frame
-	var fnName string
-
-	pc := make([]uintptr, 16)
-	_ = runtime.Callers(3+skipExtra, pc)
-	frames := runtime.CallersFrames(pc)
-
-	for i := 0; i < len(pc); i++ {
-		if frames == nil {
-			break
-		}
-
-		f, more := frames.Next()
-		fn := runtime.FuncForPC(frame.PC)
-		if fn == nil {
-			break
-		}
-		frame = f
-		fnName = fn.Name()
-		if !strings.HasPrefix(fnName, "runtime.") {
-			break
-		}
-
-		if !more {
-			break
-		}
-	}
-
-	if fnName != "" {
-		return fmt.Sprintf("[func] %v:%v", fnName, frame.Line)
-	}
-
-	if frame.File != "" {
-		return fmt.Sprintf("[file] %v:%v", frame.File, frame.Line)
-	}
-
-	return fmt.Sprintf("[pc] %x", pc)
-}
 
 // Recover 适用于需要对 err 进行重写的场景, 如 handler 中
 func Recover(handler ErrorHandler) {
-	innerRecover(handler)
-}
-
-// Recover 适用于需要对 err 进行重写的场景, 如 handler 中
-func innerRecover(handler ErrorHandler) {
 	e := recover()
 	if handler == nil || e == nil {
 		return
 	}
-	panicLoc := figurePanicLocation(1)
+	panicLoc := figurePanicLocation()
 	if err, ok := e.(error); ok {
 		handler(fmt.Errorf("%w [ panic !!! %s ]", err, panicLoc))
 	} else {
 		handler(fmt.Errorf("%v [ panic !!! %s ]", e, panicLoc))
 	}
+	//innerRecover(handler)
 }
 
+func innerRecover(handler ErrorHandler) {
+	e := recover()
+	if handler == nil || e == nil {
+		return
+	}
+	panicLoc := figurePanicLocation()
+	stack := debug.Stack()
+	//fmt.Println("stack", string(stack))
+	if err, ok := e.(error); ok {
+		handler(fmt.Errorf("%w [ panic !!! %s , stack= %s ]", err, panicLoc, string(stack)))
+	} else {
+		handler(fmt.Errorf("%v [ panic !!! %s , stack= %s ]", e, panicLoc, string(stack)))
+	}
+}
+
+// SafeGo run function in a protected goroutine
 func SafeGo(fn func(), errHandler ErrorHandler) {
 	go func() {
-		defer Recover(errHandler)
+		defer innerRecover(errHandler)
 		fn()
 	}()
-	return
+}
+
+func figurePanicLocation() string {
+	fNode := GetFrameNode(3, "runtime.")
+	return fNode.LocString()
 }
--- util/procast/recover_test.go
+++ util/procast/recover_test.go
@@ -1,20 +1,48 @@
 package procast_test
 
 import (
-	"testing"
-
 	"github.com/khicago/got/util/procast"
 	"github.com/stretchr/testify/assert"
+	"strings"
+	"sync"
+	"testing"
+	"time"
 )
 
 func TestRecover(t *testing.T) {
 	err := func() (err error) {
-		defer procast.GetRewriteErrHandler(&err).Recover()
+
+		h := procast.GetRewriteErrHandler(&err)
+		defer procast.Recover(h)
+		//defer h.Recover()
 		func() {
-			panic("recover, ok, x")
+			panic("panic 1") // should keep at line 19.
 		}()
 		return
 	}()
+
 	assert.NotNil(t, err, "recover failed")
-	assert.Equal(t, "recover, ok, x", err.Error(), "recover content failed")
+	assert.Equal(t, "panic 1 [ panic !!! [func:procast_test.TestRecover.func1.1:19] ]", err.Error(), "recover content failed")
+}
+
+func TestSafeGo(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	err := func() (err error) {
+		wg.Add(1)
+		procast.GetRewriteErrHandler(&err).SafeGo(func() {
+			defer wg.Done()
+			panic("panic 2")
+		})
+
+		wg.Wait()
+		time.Sleep(time.Millisecond)
+		return
+	}()
+
+	if !assert.NotNil(t, err, "SafeGo handle error failed") {
+		return
+	}
+
+	//fmt.Println(err.Error())
+	assert.True(t, strings.HasPrefix(err.Error(), "panic 2 [ panic !!!"), "SafeGo handle error type wrong %s", err.Error())
 }
