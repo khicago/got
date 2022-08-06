614316ebf68da7a770872701669a7b3d05a2b1c2
bagaking <zfaustk@qq.com>
2022-08-06 14:27:27 +0800

    feat (pmark): provide pmark methods

--- table2d/preset/pmark/pmark.go
+++ table2d/preset/pmark/pmark.go
@@ -1,13 +1,76 @@
 package pmark
 
-func IsPair(begin, end string) bool {
-	switch begin {
-	case `<`:
-		return end == `>`
-	case `{`:
-		return end == `}`
-	case `[`:
-		return end == `}]`
+type (
+	Mark string
+)
+
+const (
+	NIL            Mark = ""
+	BucketsAngleL  Mark = "<"
+	BucketsAngleR  Mark = ">"
+	BucketsRoundL  Mark = "("
+	BucketsRoundR  Mark = ")"
+	BucketsSquareL Mark = "["
+	BucketsSquareR Mark = "]"
+	BucketsCurlyL  Mark = "{"
+	BucketsCurlyR  Mark = "}"
+)
+
+var (
+	pairLR = map[Mark]Mark{
+		BucketsAngleL:  BucketsAngleR,
+		BucketsRoundL:  BucketsRoundR,
+		BucketsSquareL: BucketsSquareR,
+		BucketsCurlyL:  BucketsCurlyR,
 	}
-	return false
+	pairRL = map[Mark]Mark{
+		BucketsAngleR:  BucketsAngleL,
+		BucketsRoundR:  BucketsRoundL,
+		BucketsSquareR: BucketsSquareL,
+		BucketsCurlyR:  BucketsCurlyL,
+	}
+	names = map[Mark]string{
+		BucketsAngleL:  "left angle bucket",
+		BucketsAngleR:  "right angle bucket",
+		BucketsRoundL:  "left round bucket",
+		BucketsRoundR:  "right round bucket",
+		BucketsSquareL: "left square bucket",
+		BucketsSquareR: "right square bucket",
+		BucketsCurlyL:  "left curly bucket",
+		BucketsCurlyR:  "right curly bucket",
+	}
+)
+
+func (p Mark) Name() string {
+	return names[p]
+}
+
+func (p Mark) Registered() (registered bool) {
+	_, registered = names[p]
+	return
+}
+
+func (p Mark) IsLeft() (isLeft bool) {
+	_, isLeft = pairLR[p]
+	return
+}
+
+func (p Mark) Pairing() Mark {
+	r, ok := pairLR[p]
+	if ok {
+		return r
+	}
+	l, ok := pairRL[p]
+	if ok {
+		return l
+	}
+	return NIL
+}
+
+func IsPair[T ~string](begin, end T) bool {
+	r, ok := pairLR[Mark(begin)]
+	if !ok {
+		return false
+	}
+	return r == Mark(end)
 }
---
+++ table2d/preset/pmark/stack.go
@@ -0,0 +1,103 @@
+package pmark
+
+import (
+	"errors"
+	"fmt"
+	"github.com/khicago/got/util/typer"
+)
+
+type (
+	Pair[TPayload any] struct {
+		L, R       Mark
+		LVal, RVal TPayload
+	}
+
+	Stack[TPayload any] struct {
+		s       []Mark
+		p       []TPayload
+		Results []Pair[TPayload]
+	}
+)
+
+var (
+	ErrInsufficientStackLen = errors.New("insufficient stack length")
+	ErrMarkAreNotRegistered = errors.New("mark are not registered")
+)
+
+func NewStack[TPayload any](cap int) *Stack[TPayload] {
+	ret := Stack[TPayload]{}
+	if cap > 0 {
+		ret.s = make([]Mark, 0, cap)
+		ret.p = make([]TPayload, 0, cap)
+		ret.Results = make([]Pair[TPayload], 0, (cap+1)>>1)
+	} else {
+		ret.s = make([]Mark, 0)
+		ret.p = make([]TPayload, 0)
+		ret.Results = make([]Pair[TPayload], 0)
+	}
+	return &ret
+}
+
+func (s *Stack[TPayload]) Len() int {
+	return len(s.s)
+}
+
+// Push
+// only registered mark can be pushed into the stack
+func (s *Stack[TPayload]) Push(mark Mark, payload TPayload) error {
+	if !mark.Registered() {
+		return fmt.Errorf("%s %w", mark, ErrMarkAreNotRegistered)
+	}
+	s.s = append(s.s, mark)
+	s.p = append(s.p, payload)
+	return nil
+}
+
+func (s *Stack[TPayload]) Peak() (mark Mark, err error) {
+	l := s.Len()
+	if l == 0 {
+		return NIL, ErrInsufficientStackLen
+	}
+	return (s.s)[l-1], nil
+}
+
+func (s *Stack[TPayload]) Pop() (mark Mark, payload TPayload, err error) {
+	l := s.Len()
+	if l == 0 {
+		return NIL, typer.ZeroVal[TPayload](), ErrInsufficientStackLen
+	}
+	mark, s.s = (s.s)[l-1], (s.s)[:l-1]
+	payload, s.p = (s.p)[l-1], (s.p)[:l-1]
+	return mark, payload, nil
+}
+
+func (s *Stack[TPayload]) Consume(mark Mark, payload TPayload, actionPairing typer.DelegateAction2[bool, Pair[TPayload]]) error {
+	l := s.Len()
+	if l == 0 {
+		actionPairing.TryCall(false, Pair[TPayload]{L: mark, LVal: payload})
+		return s.Push(mark, payload)
+	}
+	peak, err := s.Peak()
+	if err != nil {
+		return err
+	}
+	if IsPair(peak, mark) {
+		m0, p0, err := s.Pop()
+		if err != nil {
+			return err
+		}
+
+		res := Pair[TPayload]{
+			L:    m0,
+			R:    mark,
+			LVal: p0,
+			RVal: payload,
+		}
+		s.Results = append(s.Results, res)
+
+		actionPairing.TryCall(true, res)
+		return nil
+	}
+	actionPairing.TryCall(false, Pair[TPayload]{L: mark, LVal: payload})
+	return s.Push(mark, payload)
+}
