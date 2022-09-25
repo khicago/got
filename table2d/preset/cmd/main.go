8e6b3d3383aef47da064fa9060993c0d4c1a9374
bagaking <zfaustk@qq.com>
2022-09-25 16:04:00 +0800

    feat (preset): pcol codegen cmd

---
+++ table2d/preset/cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"github.com/khicago/got/table2d/preset"
+	"github.com/khicago/got/table2d/preset/pcol"
+	"github.com/khicago/got/table2d/tablety"
+	"os"
+)
+
+func main() {
+
+	var data = tablety.WarpLineReader([][]string{
+		{"@", "ID", "INT", "Float", "[", "ID", "]", "{", "ID", "}"},
+		{" ", "link(@)", "test($>1,$<50)", "test($%2)", "link(item)", "", "", "select", "", ""},
+		{"PID", "LvUp", "Power", "Magic", "InitItems", "", "", "InnerLvUpItem", "LvUp", ""},
+		{"10001", "10002", "12", "1.2", "", "1010001", "", "", "1010003", ""},
+	})
+
+	p, _ := preset.ReadLines(context.TODO(), data)
+
+	file, err := os.OpenFile("./generated.go", os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		file, err = os.OpenFile("./table2d/preset/cmd/generated.go", os.O_WRONLY, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+	defer func(file *os.File) {
+		if err != nil {
+			err = file.Close()
+		}
+	}(file)
+	if err = pcol.GenerateCode(p.Headline, "main", "the test lass", file); err != nil {
+		panic(err)
+	}
+
+}
