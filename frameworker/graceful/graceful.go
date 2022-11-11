f45545eb880d9a40a05f0b4e65b0d217282969d2
bagaking <zfaustk@qq.com>
2022-11-12 02:44:38 +0800

    feat (frameworker): create module graceful

---
+++ frameworker/graceful/graceful.go
@@ -0,0 +1,46 @@
+package graceful
+
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/khicago/got/util/delegate"
+	"github.com/khicago/got/util/procast"
+)
+
+func GracefulServe(ctx context.Context,
+	blockAndServe delegate.Handler,
+	stop delegate.Action,
+	errorHandler procast.ErrorHandler,
+	signals ...os.Signal,
+) error {
+	ctx, cancel := WithWaitSignalAndCacel(ctx, signals...)
+	defer cancel()
+
+	procast.SafeGo(func() {
+		<-ctx.Done()
+		stop.TryCall()
+	}, errorHandler)
+	return blockAndServe()
+}
+
+func WithWaitSignal(ctx context.Context, signals ...os.Signal) context.Context {
+	ctx, _ = WithWaitSignalAndCacel(ctx, signals...)
+	return ctx
+}
+
+func WithWaitSignalAndCacel(ctx context.Context, signals ...os.Signal) (context.Context, delegate.Action) {
+	ctx, cancel := context.WithCancel(ctx)
+	procast.SafeGo(func() {
+		WaitSignal(signals...)
+		cancel()
+	}, nil)
+	return ctx, delegate.Action(cancel)
+}
+
+func WaitSignal(signals ...os.Signal) os.Signal {
+	c := make(chan os.Signal)
+	signal.Notify(c, signals...)
+	return <-c
+}
